b745b65d0d218ec33462c7d0ce7cb2561d23fb06
WONG ELEK <111182255+UWONGMUMET@users.noreply.github.com>
2024-07-21 23:26:49 +0700

    Add files via upload

---
+++ Tutorial/my-react-app/src/App.jsx
@@ -0,0 +1,15 @@
+import Header from "./Header"
+import Footer from "./Footer";
+import Food from "./Food";
+
+function App() {
+  return(
+    <div>
+      <Header></Header>
+      <Food></Food>
+      <Footer></Footer>
+    </div>
+  );
+}
+
+export default App
