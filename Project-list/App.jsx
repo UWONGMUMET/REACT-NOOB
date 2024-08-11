3705e6c215676850a72341509eb1a6b241e92ff2
WONG ELEK <111182255+UWONGMUMET@users.noreply.github.com>
2024-08-11 23:32:15 +0700

    Update App.jsx

--- Project-list/App.jsx
+++ Project-list/App.jsx
@@ -1,13 +1,24 @@
 import Form from "./GFG Projects/Form Submission/Form"
 // import JokesApp from "./GFG Projects/Jokes Generator/JokesApp"
 // import PassValid from "./GFG Projects/Password Validator/PassValid";
+// import ProjectTodoApp from "./YT Projects/TO DO APP/ToDoApp1/ProjectTodoApp"
+// import TodoApptwo from "./YT Projects/TO DO APP/TodoApp2/TodoApptwo"
+// import Nike from "./YT Projects/Nike Web/Components/Nike";
+// import Navigation from "./YT Projects/Nike Web/Components/Navigation";
 
 function App() {
     return(
         <div>
+            {/* GFG */}
             <Form></Form>
             {/* <JokesApp></JokesApp> */}
             {/* <PassValid></PassValid> */}
+            
+            {/* YT */}
+            {/* <ProjectTodoApp></ProjectTodoApp> */}
+            {/* <TodoApptwo></TodoApptwo> */}
+            {/* <Navigation></Navigation>
+            <Nike></Nike> */}
         </div>
     )
     
