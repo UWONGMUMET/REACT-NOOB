d64f9bb2c30599c213bb569e21980f40062e41d2
WONG ELEK <111182255+UWONGMUMET@users.noreply.github.com>
2024-08-11 23:26:45 +0700

    Update App.jsx

--- Project-list/App.jsx
+++ Project-list/App.jsx
@@ -1,9 +1,11 @@
 import Form from "./GFG Projects/Form Submission/Form"
+// import JokesApp from "./GFG Projects/Jokes Generator/JokesApp"
 
 function App() {
     return(
         <div>
             <Form></Form>
+            {/* <JokesApp></JokesApp> */}
         </div>
     )
     
