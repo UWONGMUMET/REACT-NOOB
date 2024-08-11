44e4cde6d50d4fbf12ddfe6d5de0ebec112e46a7
WONG ELEK <111182255+UWONGMUMET@users.noreply.github.com>
2024-08-11 23:27:52 +0700

    Update App.jsx

--- Project-list/App.jsx
+++ Project-list/App.jsx
@@ -1,11 +1,13 @@
 import Form from "./GFG Projects/Form Submission/Form"
 // import JokesApp from "./GFG Projects/Jokes Generator/JokesApp"
+// import PassValid from "./GFG Projects/Password Validator/PassValid";
 
 function App() {
     return(
         <div>
             <Form></Form>
             {/* <JokesApp></JokesApp> */}
+            {/* <PassValid></PassValid> */}
         </div>
     )
     
